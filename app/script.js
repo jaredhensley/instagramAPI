461f48cc8ec0708d34e10ca0ada3ff9153d22ce2
Jared Hensley <jahensle85@me.com>
2015-08-18 18:47:16 -0400

    update CLIENT_ID

--- app/script.js
+++ app/script.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
-	alert('test');
 
+ function getRequest() {
+ 	alert();
+    var params = {
+     count: 10
+    };
+
+    url = "https://api.instagram.com/v1/tags/coversong/media/recent?client_id=2a86eedc95bf44a691694851ae41161e";
+
+    $.get(url, params, function(results) {
+      myData = (results);
+      console.log(results);
+ 
+    });
+  }
+
+  getRequest();
 });
