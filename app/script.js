caf9a3ceeda8dc7738b72bbba5d40661f1b19668
Jared Hensley <jahensle85@me.com>
2015-08-19 18:47:49 -0400

    no link, less logs

--- app/script.js
+++ app/script.js
@@ -1,10 +1,10 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
   function getRequest(searchEntry) {
-    console.log("inside getRequest");
+      console.log("inside getRequest");
 
-   /*location.href = 'https://instagram.com/oauth/authorize/?client_id=2a86eedc95bf44a691694851ae41161e&redirect_uri=https://jaredhensley.github.io/instagramAPI/&response_type=token';*/
-      
+      location.href = 'https://instagram.com/oauth/authorize/?client_id=2a86eedc95bf44a691694851ae41161e&redirect_uri=https://jaredhensley.github.io/instagramAPI/&response_type=token';
+
       function AccessToken(value) {
         this.value = value;
         console.log("inside accessToken");
@@ -24,42 +24,37 @@ $(document).ready(function(){
           alert('null');
         }
       }
-      
-      var photoList_Instagram = [];
-      function getInstagramPhotoList(nextPageUrl) {
-        var requestUrl = 'https://api.instagram.com/v1/tags/localmusic/media/recent' + "?" + instagramToken.value;
-        console.log(requestUrl);
-        if(nextPageUrl) {
-          alert(nextPageUrl);
-          requestUrl = nextPageUrl;
-        }
-    		$.ajax({
-    			url: requestUrl,
-          type: "GET",
-    			dataType: 'jsonp',
-    			success: function(response){
-            console.log(response);
-    				response.data.forEach(function(photo) {
-              photoList_Instagram.push(photo);
-            });
-    				if (response.pagination.next_url) {
-              getInstagramPhotoList(response.pagination.next_url);
 
-            } else {
-              alert('done');
-            }
-    			} //success: callback
-    		}); // end ajax call
-      } // end getInstagramPhotoList
+      var photoList_Instagram = [];
+
+      function getInstagramPhotoList(nextPageUrl) {
+          var requestUrl = 'https://api.instagram.com/v1/tags/localmusic/media/recent' + "?" + instagramToken.value;
+          if (nextPageUrl) {
+            alert(nextPageUrl);
+            requestUrl = nextPageUrl;
+          }
+          $.ajax({
+            url: requestUrl,
+            type: "GET",
+            dataType: 'jsonp',
+            success: function(response) {
+                response.data.forEach(function(photo) {
+                  photoList_Instagram.push(photo);
+                });
+                if (response.pagination.next_url) {
+                  getInstagramPhotoList(response.pagination.next_url);
+
+                } else {
+                  alert('done');
+                }
+              } //success: callback
+          }); // end ajax call
+        } // end getInstagramPhotoList
       getInstagramAccessToken();
       getInstagramPhotoList();
-      console.log(photoList_Instagram);
-  } // end getRequest
+    } // end getRequest
   getRequest();
-
- 
+  console.log(photoList_Instagram);
 });
-
-
 
 /*access_token=30069279.2a86eed.110620b05b674816bd8de88771420a2e";*/
