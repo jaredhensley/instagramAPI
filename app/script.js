522d570f6e6a1dd67fc2b1f034b602bb4504283e
Jared Hensley <jahensle85@me.com>
2015-08-19 13:56:45 -0400

    test

--- app/script.js
+++ app/script.js
@@ -1,23 +1,14 @@
 $(document).ready(function(){
-location.href = 'https://instagram.com/oauth/authorize/?client_id=2a86eedc95bf44a691694851ae41161e&redirect_uri=https://jaredhensley.github.io/instagramAPI/&response_type=token';
+
  function getRequest(searchEntry) {
- 
-    var params = {
-     COUNT: 10,
-     type: "video"
-    };
 
-    url = "https://api.instagram.com/v1/tags/localmusic/media/recent?access_token=30069279.2a86eed.110620b05b674816bd8de88771420a2e";
-
-    // var result = $.get(url, function(results) {
-    //   myData = (results);
-    //   console.log(results);
- 
-    // });
+alert('test');
+ location.href = 'https://instagram.com/oauth/authorize/?client_id=2a86eedc95bf44a691694851ae41161e&redirect_uri=https://jaredhensley.github.io/instagramAPI/&response_type=token';
+    
+/*    url = "https://api.instagram.com/v1/tags/localmusic/media/recent?access_token=30069279.2a86eed.110620b05b674816bd8de88771420a2e";
 
 		var result = $.ajax({
 			url: url,
-      data: params,
       type: "GET",
 			dataType: 'jsonp',
 			success: function(response){
@@ -25,8 +16,8 @@ location.href = 'https://instagram.com/oauth/authorize/?client_id=2a86eedc95bf44
 				console.log(response.data);
 			}
 		});
-	}
-
+	}*/
+}
   getRequest();
  
 });
