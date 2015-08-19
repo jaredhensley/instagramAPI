ed97a857531aed05e051bebd8aa525a007a69223
Jared Hensley <jahensle85@me.com>
2015-08-19 14:32:02 -0400

    update

--- app/script.js
+++ app/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
 
- function getRequest(searchEntry) {
+/* function getRequest(searchEntry) {
 
 alert('test');
- location.href = 'https://instagram.com/oauth/authorize/?client_id=2a86eedc95bf44a691694851ae41161e&redirect_uri=https://jaredhensley.github.io/instagramAPI/&response_type=token';
+ location.href = 'https://instagram.com/oauth/authorize/?client_id=2a86eedc95bf44a691694851ae41161e&redirect_uri=https://jaredhensley.github.io/instagramAPI/&response_type=token';*/
     
-/*    url = "https://api.instagram.com/v1/tags/localmusic/media/recent?access_token=30069279.2a86eed.110620b05b674816bd8de88771420a2e";
+   url = "https://api.instagram.com/v1/tags/localmusic/media/recent?access_token=30069279.2a86eed.110620b05b674816bd8de88771420a2e";
 
 		var result = $.ajax({
 			url: url,
@@ -17,7 +17,9 @@ alert('test');
 			}
 		});
 	}*/
-}
+/*}
   getRequest();
+
+
  
 });
