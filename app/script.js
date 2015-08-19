8995e6d05a2cbd52aa0e01ff8528f9409643a153
Jared Hensley <jahensle85@me.com>
2015-08-19 14:37:05 -0400

    again

--- app/script.js
+++ app/script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 function getRequest(searchEntry) {
 
 
- location.href = 'https://instagram.com/oauth/authorize/?client_id=2a86eedc95bf44a691694851ae41161e&redirect_uri=https://jaredhensley.github.io/instagramAPI/&response_type=token';*/
+ /*location.href = 'https://instagram.com/oauth/authorize/?client_id=2a86eedc95bf44a691694851ae41161e&redirect_uri=https://jaredhensley.github.io/instagramAPI/&response_type=token';*/
     
    url = "https://api.instagram.com/v1/tags/localmusic/media/recent?access_token=30069279.2a86eed.110620b05b674816bd8de88771420a2e";
 
