529408db4487e27af1cae982d7bec715d738eb6b
Jared Hensley <jahensle85@me.com>
2015-08-19 14:35:24 -0400

    next update

--- app/script.js
+++ app/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 
-/* function getRequest(searchEntry) {
+function getRequest(searchEntry) {
 
-alert('test');
+
  location.href = 'https://instagram.com/oauth/authorize/?client_id=2a86eedc95bf44a691694851ae41161e&redirect_uri=https://jaredhensley.github.io/instagramAPI/&response_type=token';*/
     
    url = "https://api.instagram.com/v1/tags/localmusic/media/recent?access_token=30069279.2a86eed.110620b05b674816bd8de88771420a2e";
@@ -17,7 +17,7 @@ alert('test');
 			}
 		});
 	}*/
-/*}
+}
   getRequest();
 
 
