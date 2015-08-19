dffd3371a83f3eec7a6ae3e46e2798bbb36dcf4a
Jared Hensley <jahensle85@me.com>
2015-08-19 12:57:46 -0400

    redirect added

--- app/script.js
+++ app/script.js
@@ -1,15 +1,13 @@
 $(document).ready(function(){
-
+location.href = 'https://instagram.com/oauth/authorize/?client_id=2a86eedc95bf44a691694851ae41161e&redirect_uri=https://jaredhensley.github.io/instagramAPI/&response_type=token';
  function getRequest(searchEntry) {
  
     var params = {
      COUNT: 10,
-     /*Q: searchEntry*/				
-    /* q: searchEntry*/
-
+     type: "video"
     };
 
-    url = "https://api.instagram.com/v1/tags/coversongs/media/recent?access_token=30069279.2a86eed.110620b05b674816bd8de88771420a2e";
+    url = "https://api.instagram.com/v1/tags/localmusic/media/recent?access_token=30069279.2a86eed.110620b05b674816bd8de88771420a2e";
 
     // var result = $.get(url, function(results) {
     //   myData = (results);
@@ -19,10 +17,12 @@ $(document).ready(function(){
 
 		var result = $.ajax({
 			url: url,
+      data: params,
+      type: "GET",
 			dataType: 'jsonp',
 			success: function(response){
 				console.log('success:');
-				console.log(response);
+				console.log(response.data);
 			}
 		});
 	}
