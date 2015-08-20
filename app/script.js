a615a3fb930414cb0214d5a57354ed840c5369ec
Jared Hensley <jahensle85@me.com>
2015-08-20 14:03:31 -0400

    separate functions

--- app/script.js
+++ app/script.js
@@ -1,28 +1,19 @@
-function getRequest() {
-
-      console.log("inside getRequest");
 
       /*location.href = 'https://instagram.com/oauth/authorize/?client_id=2a86eedc95bf44a691694851ae41161e&redirect_uri=https://jaredhensley.github.io/instagramAPI/&response_type=token';*/
 
       function AccessToken(value) {
         this.value = value;
-        console.log("inside accessToken");
       }
 
       function getInstagramAccessToken() {
-        console.log("insideGetInstagramAccessToken");
-        console.log(location.href);
+        
         var hash = location.hash.replace('#', '');
-        console.log(location.href);
-        console.log(hash);
-        console.log(hash.indexOf("access_token"));
+      
         if (hash.indexOf("access_token") >= 0) {
           instagramToken = new AccessToken(hash);
-          console.log(instagramToken);
-          console.log(instagramToken.value);
+          getInstagramPhotoList(instagramToken);
         } else {
           instagramToken = null;
-          alert('null');
         }
       }
 
@@ -40,9 +31,9 @@ function getRequest() {
             dataType: 'jsonp',
             success: function(response) {
                 console.log(response);
-                response.data.forEach(function(photo) {
+              /*  response.data.forEach(function(photo) {
                   photoList_Instagram.push(photo);
-                });
+                })*/;
                /* if (response.pagination.next_url) {
                   getInstagramPhotoList(response.pagination.next_url);
 
@@ -52,13 +43,8 @@ function getRequest() {
             } //success: callback
           }); // end ajax call
       } // end getInstagramPhotoList
-  getInstagramAccessToken();    
-  getInstagramPhotoList();
-} // end getRequest
 
 $(document).ready(function() {
-
-  getRequest();
-
+  getInstagramAccessToken();
 });
 
