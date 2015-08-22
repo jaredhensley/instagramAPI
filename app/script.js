ea045a6a459f5d9ed002a5706056f965e9317b62
Jared Hensley <jahensle85@me.com>
2015-08-21 23:50:52 -0400

    map api added back in

--- app/script.js
+++ app/script.js
@@ -1,6 +1,6 @@
 
 function AccessToken(value) {
-  this.value = value;
+        this.value = value;
 }
 
 function getInstagramAccessToken() {
@@ -50,14 +50,11 @@ function getInstagramPhotoList(nextPageUrl) {
 $(document).ready(function() {
   getInstagramAccessToken();
   var map;
-  function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
-
-
-    });
-
-  }
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
 });
 
