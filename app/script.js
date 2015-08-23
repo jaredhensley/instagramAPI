74f61487f577c51533d8572282983aaad8e33507
Jared Hensley <jahensle85@me.com>
2015-08-22 22:09:44 -0400

    building up coords array successfully

--- app/script.js
+++ app/script.js
@@ -1,12 +1,11 @@
-
 function AccessToken(value) {
-        this.value = value;
+  this.value = value;
 }
 
 function getInstagramAccessToken() {
-        
+
   var hash = location.hash.replace('#', '');
-          
+
   if (hash.indexOf("access_token") >= 0) {
     instagramToken = new AccessToken(hash);
     getInstagramPhotoList(instagramToken);
@@ -16,46 +15,71 @@ function getInstagramAccessToken() {
 }
 
 function populateHTML(totalResults) {
-        //code here
+  //code here
   console.log(totalResults);
 }
 
 var totalResults = [];
 
 function getInstagramPhotoList(nextPageUrl) {
-  var requestUrl = 'https://api.instagram.com/v1/users/self/media/liked' + '?' + instagramToken.value;
-  /* if (nextPageUrl) {
-     requestUrl = nextPageUrl;
-     }*/
-  $.ajax({
-    url: requestUrl,
-    type: "GET",
-    dataType: 'jsonp',
-    success: function(response) {
-      console.log(response);
-      response.data.forEach(function(photo) {
-      totalResults.push(photo);
-    });
-  /*if (response.pagination.next_url) {
-    getInstagramPhotoList(response.pagination.next_url);
 
-    } else {
-      alert('done');
-    }*/
-    } //success: callback
+    var requestUrl = 'https://api.instagram.com/v1/users/self/media/liked' + '?' + instagramToken.value;
+
+    $.ajax({
+      url: requestUrl,
+      type: "GET",
+      dataType: 'jsonp',
+      success: function(response) {
+          console.log(response);
+          response.data.forEach(function(photo) {
+            totalResults.push(photo);
+          });
+        } //end success callback
     }); // end ajax call
-  populateHTML(totalResults);
-} // end getInstagramPhotoList
-  var map;
-  
+
+    populateHTML(totalResults);
+
+  } // end getInstagramPhotoList
+
+var map;
+var coords = [];
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
+    center: {lat: -34.397,lng: 150.644},
     zoom: 8
   });
+
+  var geocoder = new google.maps.Geocoder();
+
+  document.getElementById('submit').addEventListener('click', function() {
+    geocodeAddress(geocoder, map);
+
+  });
 }
+
+  function geocodeAddress(geocoder, resultsMap) {
+    var address = document.getElementById('address').value;
+    geocoder.geocode({
+      'address': address
+    }, function(results, status) {
+      if (status === google.maps.GeocoderStatus.OK) {
+        resultsMap.setCenter(results[0].geometry.location);
+        coords.push(results[0].geometry.location);
+        console.log(coords);
+        var marker = new google.maps.Marker({
+          map: resultsMap,
+          position: results[0].geometry.location
+        });
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }
+
+
 $(document).ready(function() {
+
   getInstagramAccessToken();
 
 });
-
