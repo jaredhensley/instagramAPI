e3c17a1aa43246d118b2c47bfcef10f9db613e7a
Jared Hensley <jahensle85@me.com>
2015-08-24 23:50:49 -0400

    test

--- app/script.js
+++ app/script.js
@@ -31,7 +31,7 @@ function instagramAjaxCall(instagramToken) {
 
 
 function populateHTML(totalResults) {
-  console.log(totalResults[totalResults.length-1].created_time);
+ /* console.log(totalResults[totalResults.length-1].created_time);*/
   lastCreat = totalResults[totalResults.length-1].created_time;
   console.log(totalResults);
   $.each(totalResults, function(index, value) {
