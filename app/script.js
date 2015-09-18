97791fb30e0175fd894a4f6dc4c40ed861cd0067
Jared Hensley <jahensle85@me.com>
2015-09-17 23:32:50 -0400

    scaled map markers

--- app/script.js
+++ app/script.js
@@ -1,12 +1,10 @@
 var totalResults = [];
 var markers = [];
-var coords = {lat: 40.71278, long: -74.006 };
+var coords = {lat: 40.71278, long: -74.006};
 var map;
 var lastCreat;
 var oldAddress;
 var token;
-
-
 
 function getInstagramAccessToken() {
 
@@ -19,35 +17,36 @@ function getInstagramAccessToken() {
   }
 }
 
-
 function getInstagramPhotoList(token, lastCreat, coords) {
-  var requestUrl = 'https://api.instagram.com/v1/media/search?lat=' + coords['lat'] + '&lng=' + coords['long'] + '&' + token;
-  $.ajax({
-    url: requestUrl,
-    type: "GET",
-    data: { distance: 1000, count: 20, max_timestamp: lastCreat }, 
-    dataType: 'jsonp',
-    success: function(response) {
-      response.data.forEach(function(data) {
-      totalResults.push(data);
-      });
-          
-      populateHTML(totalResults);
-      
-    } //end success callback
+    var requestUrl = 'https://api.instagram.com/v1/media/search?lat=' + coords['lat'] + '&lng=' + coords['long'] + '&' + token;
+    $.ajax({
+      url: requestUrl,
+      type: "GET",
+      data: {
+        distance: 1000,
+        count: 20,
+        max_timestamp: lastCreat
+      },
+      dataType: 'jsonp',
+      success: function(response) {
+          response.data.forEach(function(data) {
+            totalResults.push(data);
+          });
 
-  }); // end ajax call
-  clearCoords(); // resetting coords array for next search
-} // end getInstagramPhotoList
+          populateHTML(totalResults);
+
+        } //end success callback
+
+    }); // end ajax call
+    clearCoords(); // resetting coords array for next search
+  } // end getInstagramPhotoList
 
 function clearMap() {
 
-    for (var i = 0; i < markers.length; i++) {
-      markers[i].setMap(null);
-    }
-
-    markers = [];
-  
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(null);
+  }
+  markers = [];
 }
 
 function populateMap(totalResults) {
@@ -61,74 +60,66 @@ function populateMap(totalResults) {
       if (pointer.caption && pointer.caption.text) {
         return pointer.caption.text;
       } else {
-      return "test";
+        return "test";
       }
-    }  
+    }
 
-      pointer.marker = new google.maps.Marker({
-        position: position,
-        map: map,
-        title: checkForTitleText(),
-        icon: {
-          url: pointer.image,
-          size: new google.maps.Size(40,40)
-        }
+    pointer.marker = new google.maps.Marker({
+      position: position,
+      map: map,
+      title: checkForTitleText(),
+      icon: {
+        url: pointer.image,
+        size: new google.maps.Size(40, 40),
+        scaledSize: new google.maps.Size(40, 40)
+      }
     });
     markers.push(pointer.marker);
   }
 }
 
-  
 function populateHTML(totalResults) {
 
-  lastCreat = totalResults[totalResults.length-1].created_time;
+  lastCreat = totalResults[totalResults.length - 1].created_time;
   $.each(totalResults, function(index, value) {
     if (value.videos) {
       $(".results").append("<a href=" + value.videos.low_resolution.url + "><video class='video' controls><source src=" + value.videos.low_resolution.url + " type='video/mp4'> </video> </a>");
     } else {
-      $(".results").append("<a href="+value.images.standard_resolution.url +"><img class='image' src=" + value.images.standard_resolution.url + ">" + "</a>");
+      $(".results").append("<a href=" + value.images.standard_resolution.url + "><img class='image' src=" + value.images.standard_resolution.url + ">" + "</a>");
     }
   });
   populateMap(totalResults);
 }
 
-
-function clearResults () {
+function clearResults() {
   $(".results").html("");
   totalResults = [];
 }
 
-
-function clearGeoSearch () {
-  $("#address").val(""); 
+function clearGeoSearch() {
+  $("#address").val("");
 }
 
-
-function clearCoords () {
+function clearCoords() {
   coords = {};
 }
 
-
 function initMap() {
-  
+
   map = new google.maps.Map(document.getElementById('location-canvas'), {
     center: {
       lat: 40.71278,
       lng: -74.006
     },
-    zoom:  11
+    zoom: 11
   });
 
   var geocoder = new google.maps.Geocoder();
 
   $('.ion-eye').on('keydown click', function() {
-    geocodeAddress(geocoder, map); 
+    geocodeAddress(geocoder, map);
   });
-
-  /*google.maps.event.addDomListener(window, 'resize', initMap);
-  google.maps.event.addDomListener(window, 'load', initMap);
-*/}
-
+}
 
 function checkIfNewAddress(address) {
   if (address !== oldAddress) {
@@ -138,7 +129,6 @@ function checkIfNewAddress(address) {
     oldAddress = address;
   }
 }
-
 
 function geocodeAddress(geocoder, resultsMap) {
   clearCoords();
@@ -161,30 +151,29 @@ function geocodeAddress(geocoder, resultsMap) {
       alert('Geocode was not successful for the following reason: ' + status);
     }
   });
-  
-  clearGeoSearch();
-  clearResults();  
-  
-}
 
+  clearGeoSearch();
+  clearResults();
+
+}
 
 $(document).ready(function() {
 
-  if (location.hash.indexOf("access_token") > 0 ) {
+  if (location.hash.indexOf("access_token") > 0) {
     getInstagramAccessToken();
   } else {
-  document.location = 'https://instagram.com/oauth/authorize/?client_id=2a86eedc95bf44a691694851ae41161e&redirect_uri=https://jaredhensley.github.io/instagramAPI/&response_type=token';
+    document.location = 'https://instagram.com/oauth/authorize/?client_id=2a86eedc95bf44a691694851ae41161e&redirect_uri=https://jaredhensley.github.io/instagramAPI/&response_type=token';
   }
 
   var $overlay = $("<div id='overlay'></div>");
-  var $image =  $("<img>");
+  var $image = $("<img>");
   var $video = $("<video controls type='video/mp4'></video>");
 
   $overlay.append($image);
   $overlay.append($video);
   $("body").append($overlay);
-  
-  $(".results").on("click", "a", function(event){
+
+  $(".results").on("click", "a", function(event) {
     event.preventDefault();
     var href = $(this).attr("href");
     if (href.indexOf("jpg") > 0) {
